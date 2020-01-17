91c13e3f9808052293cb73bda02063fc123dab1e
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2020-01-16 22:47:53 -0600

    Improved YahooOpenCloseScraper output format, and added error catching

--- Yahoo_Scraper.py
+++ Yahoo_Scraper.py
@@ -13,24 +13,33 @@ def YahooOpenCloseScraper(stock_id, start_date, end_date):
 
     #getting cookies and crumb
     url = f'https://finance.yahoo.com/quote/{stock_id}/history'
-    with requests.session():
-        header = {'Connection': 'keep-alive',
-                  'Expires': '-1',
-                  'Upgrade-Insecure-Requests': '1',
-                  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
-                       AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
-                  }
-    website = requests.get(url, headers=header)
-    cookie = website.cookies
-    soup = BeautifulSoup(website.text, 'lxml')
-    crumb = re.findall('"CrumbStore":{"crumb":"(.+?)"}', str(soup))
+    #make sure crumb is found
+    len = -1
+    while len < 0:
+        with requests.session():
+            header = {'Connection': 'keep-alive',
+                      'Expires': '-1',
+                      'Upgrade-Insecure-Requests': '1',
+                      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+                           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
+                      }
+        website = requests.get(url, headers=header)
+        cookie = website.cookies
+        soup = BeautifulSoup(website.text, 'lxml')
+        crumb = re.findall('"CrumbStore":{"crumb":"(.+?)"}', str(soup))
+        len = crumb.__len__()
 
     #getting download file between desired dates
     url = f'https://query1.finance.yahoo.com/v7/finance/download/{stock_id}?period1={start_unix}&period2={end_unix}&interval=1d&events=history&crumb={crumb[0]}'
     website = requests.get(url, headers=header, cookies=cookie)
     text = website.text
+    df = [x.split(',') for x in text.split('\n')]
+    df[0].insert(0, 'Symbol')
+    for x in df[1:]:
+        x.insert(0, stock_id)
+    df.pop(df.__len__()-1)
 
-    return text
+    return df
 
 
 
